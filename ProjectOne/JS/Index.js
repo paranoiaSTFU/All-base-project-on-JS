30cb15edfb92bad602f5110af793d7b3c2cc2198
Artem <106353839+paranoiaSTFU@users.noreply.github.com>
2024-07-21 14:07:56 +0300

    Add JS navigation
    Add JavaScript navigation

---
+++ ProjectOne/JS/Index.js
@@ -0,0 +1,21 @@
+function HrefOnListOfPlaybleRaces(){
+    window.location.href = '../HTML/ListOfPlaybleRaces.html';
+};
+function HrefOnAbout(){
+    window.location.href = '../HTML/About.html';
+};
+function HrefOnContacts(){
+    window.location.href = '../HTML/Contacts.html';
+};
+function HrefOnCharacterSheet(){
+    window.location.href = '../HTML/CharacterSheet.html';
+};
+function HrefOnIndex(){
+    window.location.href = '../HTML/Index.html';
+};
+function HrefOnRulesOfTheGame(){
+    window.location.href = '../HTML/RulesOfTheGame.html';
+};
+function HrefOnSupport(){
+    window.location.href = '../HTML/Support.html';
+};
